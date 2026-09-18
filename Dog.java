4739dcca5a8fbb3c6c4288d3b2a251c816fccda5
GudSkillsBadPC <kevin111411@icloud.com>
2026-09-17 18:11:08 -0700

    more files

---
+++ Dog.java
@@ -0,0 +1,25 @@
+package Java;
+
+public class Dog {
+    private int age;
+    private String name;
+    private String breed;
+
+    public Dog(int dogage, String dogname, String dogbreed) {
+        age = dogage;
+        name = dogname;
+        breed = dogbreed;
+    }
+
+    public int getage() {
+        return age;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public String breed() {
+        return breed;
+    }
+}

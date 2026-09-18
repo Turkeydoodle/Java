33f327a1e10a2f5ffa656d8fb525fe3cb00b21ba
GudSkillsBadPC <kevin111411@icloud.com>
2026-09-17 18:28:56 -0700

    new dog class

--- Dog.java
+++ Dog.java
@@ -19,7 +19,17 @@ public class Dog {
         return name;
     }
 
-    public String breed() {
+    public String getbreed() {
         return breed;
+    }
+
+    public static void main(String[] arg) {
+        Dog dog1 = new Dog(67, "Connor", "Retriever");
+        int ageofdog = dog1.getage();
+        String nameofdog = dog1.getname();
+        String breedofdog = dog1.getbreed();
+        System.out.println(ageofdog);
+        System.out.println(nameofdog);
+        System.out.println(breedofdog);
     }
 }
